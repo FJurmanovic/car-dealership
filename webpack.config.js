396ecf5b9378d87df308a4bd9fb3bbf6f8c6c781
Fran Jurmanović <fjurma12@outlook.com>
2020-06-17 19:14:40 +0200

    Added babel preset for mobx and turned on experimental decorators in jsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
-                    query: {"plugins": ["transform-decorators-legacy", "transform-class-properties"]}
+                    loader: 'babel-loader'
                 }
             },
             {
